da2d905aaf58e03bd0eecbc809a07e9370fc1a0b
Yasin <ugurluy21@itu.edu.tr>
2024-10-29 21:47:35 +0300

    Add category,author and books models with authors serialzer

--- BookApp/views.py
+++ BookApp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Author
+from rest_framework.response import Response
+from .serializers import AuthorSerializer
+from rest_framework import status
+ 
+class AuthorView(APIView):
+    def get(self,request,id):
+        try:
+            author = Author.objects.get(id=id)
+            data = AuthorSerializer(author).data
+            return Response({
+                'data': data  
+            }, status=status.HTTP_200_OK) 
+        except:
+            return Response({'error': 'Book not found'}, status=404)
+        
+        
 
-# Create your views here.
